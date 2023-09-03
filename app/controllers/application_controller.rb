f8fb9518d1f4f800cc67ced144c2fa70ee36c720
fukumochi-0110 <fukumoto08@gmail.com>
2023-09-04 01:18:49 +0900

    サインイン後のページ遷移先を記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def after_sign_in_path_for(resource)
+    if current_user
+      flash[:notice] = "ログインに成功しました" 
+      root_path
+    end
+  end
 end
