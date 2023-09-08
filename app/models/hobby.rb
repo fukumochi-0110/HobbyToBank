24a6e6803da3dfd0801774b566e53eaaf8cdfae5
fukumochi-0110 <fukumoto08@gmail.com>
2023-09-09 00:27:00 +0900

    hobbyモデル記述

--- app/models/hobby.rb
+++ app/models/hobby.rb
@@ -1,5 +1,7 @@
 class Hobby < ApplicationRecord
-  def index
-    
-  end
+  validates :hobby,presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  #belongs_to :bank
+  belongs_to :user
 end
