59c63fc0b926265d71dc3898869aa2a7dbb1af62
fukumochi-0110 <fukumoto08@gmail.com>
2023-09-09 00:25:01 +0900

    コントローラー記述

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -1,2 +1,25 @@
 class HobbiesController < ApplicationController
+  def index
+    @hobby = Hobby.all
+  end
+
+  def new
+    @hobby = Hobby.new
+  end
+
+  def create
+    @hobby = Hobby.new(hobby_params)
+    if @hobby.save
+      redirect_to hobbies_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  
+  def hobby_params
+    params.require(:hobby).permit(:hobby, :price).merge(user_id: current_user.id)
+  end
 end
